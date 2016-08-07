cd3b07f6a2c31b57788ea157f27b889178e6d743
Jim Cummins <jimthedev@gmail.com>
2016-08-06 22:30:46 -0500

    Use correct port in dev and prod

--- src/stores.jsx
+++ src/stores.jsx
@@ -1,11 +1,20 @@
 // npm install --save @horizon/client
 import Horizon from '@horizon/client';
 
+let port;
+if (process.env.NODE_ENV === 'production') {
+  port = 80;
+} else {
+  port = 8181;
+}
+
+const host = window.location.hostname + ':' + port;
+console.log('host is ', host);
 // Create an instance of Horizon, passing a config object
 const horizon = Horizon({
   secure: false,
   // host: 'localhost:8181'
-  host: window.location.hostname + ':80'
+  host: host
 });
 
 // Any number of stores that you like
