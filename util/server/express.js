859e886498064582baf9100667dafdd283a73303
Jim Cummins <jimthedev@gmail.com>
2016-08-10 00:19:51 -0500

    Add react router

--- util/server/express.js
+++ util/server/express.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname + '/../../public')));
 
-app.get('/', function(req, res) {
+app.use(function(req, res) {
   res.sendFile(path.resolve(__dirname + '/../../public/index.html'));
 });
 const httpServer = app.listen(process.env.PORT);
